bf314fb5e0f30bbc1dda348f4ce29f79db242a6d
hendisantika <hendisantika@yahoo.co.id>
2025-09-15 05:56:52 +0700

    feat: Add updateProductById in ProductService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/ecommerceapp2/service/ProductService.java
+++ src/main/java/id/my/hendisantika/ecommerceapp2/service/ProductService.java
@@ -3,8 +3,16 @@ package id.my.hendisantika.ecommerceapp2.service;
 import id.my.hendisantika.ecommerceapp2.entity.Product;
 import id.my.hendisantika.ecommerceapp2.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.Optional;
 
@@ -53,5 +61,44 @@ public class ProductService {
     public Product getProductById(long id) {
         // TODO Auto-generated method stub
         return productRepository.findById(id).orElse(null);
+    }
+
+    public Product updateProductById(Product product, MultipartFile file) {
+        Product dbProductById = getProductById(product.getId());
+
+        String imageName = file.isEmpty() ? dbProductById.getProductImage() : file.getOriginalFilename();
+        dbProductById.setProductImage(imageName);
+        dbProductById.setProductTitle(product.getProductTitle());
+        dbProductById.setProductDescription(product.getProductDescription());
+        dbProductById.setProductCategory(product.getProductCategory());
+        dbProductById.setProductPrice(product.getProductPrice());
+        dbProductById.setProductStock(product.getProductStock());
+        dbProductById.setCreatedAt(product.getCreatedAt());
+        dbProductById.setIsActive(product.getIsActive());
+        //discount logic
+        dbProductById.setDiscount(product.getDiscount());
+        Double discount = product.getProductPrice() * (product.getDiscount() / 100.0);
+        Double discountPrice = product.getProductPrice() - discount;
+        dbProductById.setDiscountPrice(discountPrice);
+
+        Product updatedProduct = productRepository.save(dbProductById);
+
+        //product save then we need to save our new updated image
+        if (!ObjectUtils.isEmpty(updatedProduct)) {
+            if (!file.isEmpty()) {
+                try {
+                    File savefile = new ClassPathResource("static/img").getFile();
+                    Path path = Paths.get(savefile.getAbsolutePath() + File.separator + "product_image" + File.separator + file.getOriginalFilename());
+                    System.out.println("File save Path :" + path);
+                    Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return updatedProduct;
+        }
+        return null;
     }
 }
