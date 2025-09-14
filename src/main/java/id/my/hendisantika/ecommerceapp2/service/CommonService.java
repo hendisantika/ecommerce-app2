fa912105c1314bb77551ed27dc5cdc179c9f2401
hendisantika <hendisantika@yahoo.co.id>
2025-09-15 05:53:02 +0700

    feat: Add CommonService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/ecommerceapp2/service/CommonService.java
@@ -0,0 +1,30 @@
+package id.my.hendisantika.ecommerceapp2.service;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : ecommerce-app2
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 15/09/25
+ * Time: 05.52
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class CommonService {
+    public void removeSessionMessage() {
+        HttpServletRequest request = ((ServletRequestAttributes) (RequestContextHolder.getRequestAttributes())).getRequest();
+        HttpSession session = request.getSession();
+
+        session.removeAttribute("successMsg");
+        session.removeAttribute("errorMsg");
+
+    }
+}
