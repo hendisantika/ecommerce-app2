049e59cc1cca994ee6adb4d39baf2f44065fb32e
hendisantika <hendisantika@yahoo.co.id>
2025-09-15 06:01:51 +0700

    feat: Add AppConstant🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/ecommerceapp2/util/AppConstant.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.ecommerceapp2.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : ecommerce-app2
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 15/09/25
+ * Time: 06.01
+ * To change this template use File | Settings | File Templates.
+ */
+public class AppConstant {
+    //public static final long UNLOCK_DURATION_TIME = 1*60*60*1000;
+    public static final long UNLOCK_DURATION_TIME = 600000; //10mins
+
+    public static final long ATTEMPT_COUNT = 3; //account will be lock after 3 Failed Attempts
+
+}
