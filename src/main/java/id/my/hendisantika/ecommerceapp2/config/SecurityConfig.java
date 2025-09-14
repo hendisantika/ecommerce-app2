616dd7af7bb86113e5225257dd74b11eac017f61
hendisantika <hendisantika@yahoo.co.id>
2025-09-15 06:20:08 +0700

    feat: Add SecurityConfig🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/ecommerceapp2/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package id.my.hendisantika.ecommerceapp2.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : ecommerce-app2
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 15/09/25
+ * Time: 06.19
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final AuthenticationSuccessHandler authenticationSuccessHandler;
+
+    @Lazy
+    private final AuthenticationFailureHandler authenticationFailureHandler;
+}
