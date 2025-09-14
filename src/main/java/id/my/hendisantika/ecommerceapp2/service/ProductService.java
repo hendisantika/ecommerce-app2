b622654fc28a047ad807185e37dc4dac94bafbef
hendisantika <hendisantika@yahoo.co.id>
2025-09-15 05:55:09 +0700

    feat: Add ProductService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/ecommerceapp2/service/ProductService.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.ecommerceapp2.service;
+
+import id.my.hendisantika.ecommerceapp2.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : ecommerce-app2
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 15/09/25
+ * Time: 05.54
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+}
