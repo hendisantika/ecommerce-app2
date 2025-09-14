e697f06de45c7a82bc3f1a70788c15074e3f1dd3
hendisantika <hendisantika@yahoo.co.id>
2025-09-15 05:41:31 +0700

    feat: Add ProductOrder Entity🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/ecommerceapp2/entity/ProductOrder.java
@@ -0,0 +1,60 @@
+package id.my.hendisantika.ecommerceapp2.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Date;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : ecommerce-app2
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 15/09/25
+ * Time: 05.40
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ProductOrder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderId;
+    private Date orderDate;
+
+    @ManyToOne
+    private Product product;
+
+    private Double price;
+
+    private Integer quantity;
+
+    @ManyToOne
+    private User user;
+
+    private String status;
+
+    private String paymentType;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private OrderAddress orderAddress;
+}
