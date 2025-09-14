009e19194e959a4f08ab8741a2f2cd85ed496e5b
hendisantika <hendisantika@yahoo.co.id>
2025-09-15 06:03:05 +0700

    feat: Add sendEmail in CommonUtils🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/ecommerceapp2/util/CommonUtils.java
+++ src/main/java/id/my/hendisantika/ecommerceapp2/util/CommonUtils.java
@@ -1,8 +1,13 @@
 package id.my.hendisantika.ecommerceapp2.util;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+
+import java.io.UnsupportedEncodingException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +25,22 @@ import org.springframework.stereotype.Component;
 public class CommonUtils {
 
     private final JavaMailSender mailSender;
+
+    public Boolean sendEmail(String url, String reciepentEmail) throws UnsupportedEncodingException, MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(message);
+
+        messageHelper.setFrom("myplacementjourney@gmail.com", "RED WINGS");
+        messageHelper.setTo(reciepentEmail);
+
+        String content = "<p>Hello, </p>" + "<p> You have requested to reset your password. </p> "
+                + "<p>Please Click the link to change your password:</p>" + "<p><a href=\"" + url + "\">Change my password</a></p>";
+
+        messageHelper.setSubject("Password Reset for RED WINGS");
+        messageHelper.setText(content, true);
+
+        mailSender.send(message);
+
+        return true;
+    }
 }
