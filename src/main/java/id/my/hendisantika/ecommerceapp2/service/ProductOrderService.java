fe1320a1e58d0ce377b271bfa681f3e5cee5203c
hendisantika <hendisantika@yahoo.co.id>
2025-09-15 05:54:24 +0700

    feat: Add saveProductOrder in ProductOrderService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/ecommerceapp2/service/ProductOrderService.java
+++ src/main/java/id/my/hendisantika/ecommerceapp2/service/ProductOrderService.java
@@ -1,9 +1,16 @@
 package id.my.hendisantika.ecommerceapp2.service;
 
+import id.my.hendisantika.ecommerceapp2.entity.Cart;
+import id.my.hendisantika.ecommerceapp2.entity.ProductOrder;
+import id.my.hendisantika.ecommerceapp2.entity.ProductOrderRequest;
 import id.my.hendisantika.ecommerceapp2.repository.CartRepository;
 import id.my.hendisantika.ecommerceapp2.repository.ProductOrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +29,25 @@ public class ProductOrderService {
     private final ProductOrderRepository productOrderRepository;
 
     private final CartRepository cartRepository;
+
+    public ProductOrder saveProductOrder(Long userId, ProductOrderRequest productOrderRequest) {
+        //get which products are order by user
+        List<Cart> listOfCarts = cartRepository.findByUserId(userId);
+        for (Cart cart : listOfCarts) {
+
+            ProductOrder order = new ProductOrder();
+
+            order.setOrderId(UUID.randomUUID().toString());
+            order.setOrderDate(new Date());
+
+            order.setProduct(cart.getProduct());
+            order.setPrice(cart.getProduct().getDiscountPrice());
+            order.setQuantity(cart.getQuantity());
+
+            order.setUser(cart.getUser());
+            order.setStatus("In Progress");
+        }
+
+        return null;
+    }
 }
