600a05b1ece4749c06fb4afa4569ad68b4ee66b9
hendisantika <hendisantika@yahoo.co.id>
2025-09-15 06:20:42 +0700

    feat: Add userDetailsService in SecurityConfig🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/ecommerceapp2/config/SecurityConfig.java
+++ src/main/java/id/my/hendisantika/ecommerceapp2/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
@@ -32,5 +33,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsServiceImpl();
     }
 }
