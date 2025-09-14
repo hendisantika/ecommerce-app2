c3ab3702c6bd080bb838f64f46d5cc9cd158a664
hendisantika <hendisantika@yahoo.co.id>
2025-09-15 05:38:43 +0700

    feat: Add Cart Entity🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/ecommerceapp2/entity/Cart.java
@@ -0,0 +1,52 @@
+package id.my.hendisantika.ecommerceapp2.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Transient;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.apache.catalina.User;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : ecommerce-app2
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 15/09/25
+ * Time: 05.38
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Product product;
+
+    private Integer quantity;
+
+    @Transient
+    private Double totalPrice; //Transient :this column will not created in DB. just use for UI purpose.
+
+    @Transient
+    private Double totalOrderPrice; //Transient :this column will not created in DB. just use for UI purpose.
+}
