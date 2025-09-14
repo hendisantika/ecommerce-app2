baa8c4695aa9838215674fdd831e252952854f68
hendisantika <hendisantika@yahoo.co.id>
2025-09-15 05:46:51 +0700

    feat: Add saveCart in CartService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/ecommerceapp2/service/CartService.java
+++ src/main/java/id/my/hendisantika/ecommerceapp2/service/CartService.java
@@ -1,10 +1,14 @@
 package id.my.hendisantika.ecommerceapp2.service;
 
+import id.my.hendisantika.ecommerceapp2.entity.Cart;
+import id.my.hendisantika.ecommerceapp2.entity.Product;
+import id.my.hendisantika.ecommerceapp2.entity.User;
 import id.my.hendisantika.ecommerceapp2.repository.CartRepository;
 import id.my.hendisantika.ecommerceapp2.repository.ProductRepository;
 import id.my.hendisantika.ecommerceapp2.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,4 +29,30 @@ public class CartService {
     private final ProductRepository productRepository;
 
     private final UserRepository userRepository;
+
+    public Cart saveCart(Long productId, Long userId) {
+        User user = userRepository.findById(userId).get();
+        Product product = productRepository.findById(productId).get();
+
+        Cart cartStatus = cartRepository.findByProductIdAndUserId(productId, userId);
+
+        Cart cart = null;
+
+        if (ObjectUtils.isEmpty(cartStatus)) {
+            //cart Is empty - so add product to Cart
+            cart = new Cart();
+            cart.setUser(user);
+            cart.setProduct(product);
+            cart.setQuantity(1);
+            cart.setTotalPrice(1 * product.getDiscountPrice());
+        } else {
+            //cart Is holding product. so increases the quantity of the existing product.
+            cart = cartStatus;
+            cart.setQuantity(cart.getQuantity() + 1);
+            cart.setTotalPrice(cart.getQuantity() * cart.getProduct().getDiscountPrice());
+        }
+        Cart saveCart = cartRepository.save(cart);
+
+        return saveCart;
+    }
 }
