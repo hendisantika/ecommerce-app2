4ecbc7b106830235a409bdfe1870b96b38684ecf
hendisantika <hendisantika@yahoo.co.id>
2025-09-15 06:25:51 +0700

    feat: Add getUserDetails in AdminViewController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/ecommerceapp2/controller/AdminViewController.java
+++ src/main/java/id/my/hendisantika/ecommerceapp2/controller/AdminViewController.java
@@ -1,12 +1,20 @@
 package id.my.hendisantika.ecommerceapp2.controller;
 
+import id.my.hendisantika.ecommerceapp2.entity.Category;
+import id.my.hendisantika.ecommerceapp2.entity.User;
 import id.my.hendisantika.ecommerceapp2.service.CartService;
 import id.my.hendisantika.ecommerceapp2.service.CategoryService;
 import id.my.hendisantika.ecommerceapp2.service.ProductService;
 import id.my.hendisantika.ecommerceapp2.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,6 +27,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Time: 06.24
  * To change this template use File | Settings | File Templates.
  */
+@Slf4j
 @Controller
 @RequestMapping("/admin")
 @RequiredArgsConstructor
@@ -32,4 +41,24 @@ public class AdminViewController {
 
     private final CartService cartService;
 
+    //to track which user is login right Now
+    //by default call this method when any request come to this controller because of @ModelAttribut
+    @ModelAttribute
+    public void getUserDetails(Principal principal, Model model) {
+        if (principal != null) {
+            String currenLoggedInUserEmail = principal.getName();
+            User currentUserDetails = userService.getUserByEmail(currenLoggedInUserEmail);
+            //System.out.println("Current Logged In User is :: ADMIN Controller :: "+currentUserDetails.toString());
+            model.addAttribute("currentLoggedInUserDetails", currentUserDetails);
+
+            //for showing user cart count
+            Long countCartForUser = cartService.getCounterCart(currentUserDetails.getId());
+            log.info("Admin Cart Count :{}", countCartForUser);
+            model.addAttribute("countCartForUser", countCartForUser);
+
+        }
+        List<Category> allActiveCategory = categoryService.findAllActiveCategory();
+        model.addAttribute("allActiveCategory", allActiveCategory);
+
+    }
 }
