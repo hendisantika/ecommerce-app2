de74827a6433b8d6e721304c2f7f2032e54ff7c3
hendisantika <hendisantika@yahoo.co.id>
2025-09-16 06:33:54 +0700

    fix: Codes :four:🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/ecommerceapp2/entity/Category.java
+++ src/main/java/id/my/hendisantika/ecommerceapp2/entity/Category.java
@@ -1,5 +1,6 @@
 package id.my.hendisantika.ecommerceapp2.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -36,6 +37,8 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
     private String categoryName;
     private String categoryImage;
     private Boolean isActive;
